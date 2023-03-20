de1e3f076cc58bcb97fb448d3fcb05ed4cd4d918
ashar <logicriuscon@gmail.com>
2023-03-20 08:05:59 +0700

    menambahkan class SimpleDataBase
    menambahkan class SimpleDataBase

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using modul5_1302210070;
+
+class MainProgram
+{
+    static void Main(string[] args)
+    {
+        SimpleDataBase<long> database = new SimpleDataBase<long>();
+        database.addNewData(13, DateTime.Now);
+        database.addNewData(02, DateTime.Now);
+        database.addNewData(21, DateTime.Now);
+        
+        database.printAllData();
+
+    }
+}
